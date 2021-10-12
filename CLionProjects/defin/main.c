d36110e295c8d945e398236c38c9f252d355e2d0
lqc <2807652103@qq.com>
2021-10-12 20:22:55 +0800

    define

--- CLionProjects/defin/main.c
+++ CLionProjects/defin/main.c
@@ -4,9 +4,10 @@
 /*反斜杠可以可以让宏定义换行*/
 #define XXX  "jjjjjjjjjjjjjjjjj\
 zzzzzz"
+#define XName(n) x##n
+#define Print_XN(n) printf("x"#n"=%d\n",x##n)
 int main() {
     printf("%s\n",XXX);
-
     /*c11可以，c99b不可*/
     int x=5;
     int z;
@@ -16,5 +17,12 @@ int main() {
     PR(z);
     z=SQUARE(2);
     printf("Evaluating SQUARE(2):");
+
+    int XName(1)=14;//相当于x1=14
+    int XName(2)=20;//相当于x2=20
+    int x3=20;
+    Print_XN(1);
+    Print_XN(2);
+    Print_XN(3);
     return 0;
 }
