256c1fb1aec14178ba5a6b6dcedeed488a0276e8
lqc <2807652103@qq.com>
2021-10-15 23:02:13 +0800

    fsd

--- 文档/raft/src/main/java/ddd/Server.java
+++ 文档/raft/src/main/java/ddd/Server.java
@@ -10,13 +10,59 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Random;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    static String sss="";
+    static void t(Random random ,String s) throws IOException {
+        Socket  socket = new Socket(s, 9001);
+        int num=random.nextInt(2);
+        System.out.println(num);
+        OutputStream outputStream=socket.getOutputStream();
+        outputStream.write(num);
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
         InetAddress addr = InetAddress.getByName("main");
         final ServerSocket ss = new ServerSocket(9001, 1000, addr);
         final HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
 
+        Thread t1=
+        new Thread(){
+            @Override
+            public void run() {
+                String s0="main";
+                String s1="node1";
+                String s2="node2";
+                String s3="node3";
+                Random random=new Random();
+                try {
+
+                    t(random,s0);
+                    System.out.println("已连接"+s0);
+                }catch (Exception e){
+                    System.out.println("没有连接上"+s0);
+                }
+                try {
+                    t(random,s1);
+                    System.out.println("已连接"+s1);
+                }catch (Exception e){
+                    System.out.println("没有连接上"+s1);
+                }
+                try {
+                    t(random,s2);
+                    System.out.println("已连接"+s2);
+                }catch (Exception e){
+                    System.out.println("没有连接上"+s2);
+                }
+                try {
+                    t(random,s3);
+                    System.out.println("已连接"+s3);
+                }catch (Exception e){
+                    System.out.println("没有连接上"+s3);
+                }
+            }
+        };
+        Thread t2=
         new Thread() {
             @Override
             public void run() {
@@ -41,15 +87,18 @@ public class Server {
                             }
                             System.out.println(str);
                         }
-
+                        if (str.contains(" to be propser")){
+                            sss=str;
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                     System.out.println("收到");
                 }
             }
-        }.start();
+        };
 
+        Thread t3=
         new Thread() {
             @Override
             public void run() {
@@ -66,7 +115,7 @@ public class Server {
                             }
                         }
                     }
-                    if (tcount >= (hashMap.size() / 2 + 1)) {
+                    if ((tcount >= (hashMap.size() / 2 + 1)&&!sss.contains(" to be propser"))) {
                         System.out.println(ss.getInetAddress().toString() + ":" + ss.getLocalPort() + " to be propser");
                         for (String key : hashMap.keySet()) {
                             String ks = key;
@@ -85,10 +134,17 @@ public class Server {
                     } else if(fcount >= (hashMap.size() / 2 + 1)){
                         System.out.println(ss.getInetAddress().toString() + ":" + ss.getLocalPort() + " is leaner");
                         fcount=0;
+                        hashMap.clear();
                     }
                 }
             }
-        }.start();
+        };
+
+        t2.start();
+        Thread.sleep(1000);
+        t1.start();
+        Thread.sleep(1000);
+        t3.start();
         }
     }
 
