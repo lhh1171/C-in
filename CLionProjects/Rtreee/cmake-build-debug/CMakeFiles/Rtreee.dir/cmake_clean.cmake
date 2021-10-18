283da5e4fc9e62b88e1017a1d8b50a99f6053334
lqc <2807652103@qq.com>
2021-10-18 21:21:18 +0800

    rr

---
+++ CLionProjects/Rtreee/cmake-build-debug/CMakeFiles/Rtreee.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rtreee.dir/main.cpp.o"
+  "Rtreee"
+  "Rtreee.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rtreee.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CLionProjects/Rtreee/cmake-build-debug/CMakeFiles/Rtreee.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Rtreee.
+# This may be replaced when dependencies are built.
