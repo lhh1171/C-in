10ebb30b223d627938ca552670cb8c1530340b74
lqc <2807652103@qq.com>
2021-10-23 22:17:36 +0800

    ,re,

---
+++ CLionProjects/Rtree2/cmake-build-debug/CMakeFiles/Rtree2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rtree2.dir/main.cpp.o"
+  "Rtree2"
+  "Rtree2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rtree2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CLionProjects/Rtree2/cmake-build-debug/CMakeFiles/Rtree2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Rtree2.
+# This may be replaced when dependencies are built.
